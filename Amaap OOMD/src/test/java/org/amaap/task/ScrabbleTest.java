69d94dee2565c335e9943fa4369ab6f154e0c2a8
Sudhir Takale <sudhirtakale99@gmail.com>
2024-03-15 17:28:28 +0530

    Invalid string exception added

--- Amaap OOMD/src/test/java/org/amaap/task/ScrabbleTest.java
+++ Amaap OOMD/src/test/java/org/amaap/task/ScrabbleTest.java
@@ -1,21 +1,30 @@
 package org.amaap.task;
 
+import org.amaap.task.exceptions.InvalidStringException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ScrabbleTest {
 
+
     @Test
-    void shouldReturnZeroIfEmptyStringIsPassed() {
+    void shouldReturnZeroIfEmptyStringIsPassed() throws InvalidStringException {
 
-        Scrabble scrabble = new Scrabble("");
-
-        int result = scrabble.calculateResult();
-
-        Assertions.assertEquals(0, result);
-
+        Assertions.assertThrows(InvalidStringException.class, () -> {
+            new Scrabble(" ");
+        });
 
     }
+
+    @Test
+    void shouldReturnInvalidStringExceptionIfStringContainsNumbersOrChars() {
+        Assertions.assertThrows(InvalidStringException.class, () -> {
+            new Scrabble("Sudhir99");
+        });
+    }
+
+
+
 
 
 }
