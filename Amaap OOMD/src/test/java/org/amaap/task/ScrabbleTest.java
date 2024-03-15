232648f443db139797326de31aee6c7f5dd75fb4
Sudhir Takale <sudhirtakale99@gmail.com>
2024-03-15 12:55:59 +0530

    done with empty string passed testcase

--- Amaap OOMD/src/test/java/org/amaap/task/ScrabbleTest.java
+++ Amaap OOMD/src/test/java/org/amaap/task/ScrabbleTest.java
@@ -1,7 +1,21 @@
 package org.amaap.task;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ScrabbleTest {
 
+    @Test
+    void shouldReturnZeroIfEmptyStringIsPassed() {
+
+        Scrabble scrabble = new Scrabble("");
+
+        int result = scrabble.calculateResult();
+
+        Assertions.assertEquals(0, result);
+
+
+    }
 
 
 }
