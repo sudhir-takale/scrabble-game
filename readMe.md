71695dfd61badf3edbc835b21e86dddcee1db517
Sudhir Takale <sudhirtakale99@gmail.com>
2024-03-20 18:43:24 +0530

    added readme file

---
+++ readMe.md
@@ -0,0 +1,24 @@
+# Scrabble
+
+Scrabble, this is a game where players attempt to create words from a set of letter tiles. Different letters have different points allocated to them.
+In the English alphabet, letters have the following point scores:
+Point(s)	Letter(s)
+1			E, A, I, O, N, R, T, L, S, U
+2			D, G
+3			B, C, M, P
+4			F, H, V, W, Y
+5			K
+8			J, X
+10			Q, Z
+
+Calculate the score for a word. The score is the sum of the points for the letters that make up a word.
+For example: GUARDIAN = 2 + 1 + 1 + 1 + 2 + 1 + 1 + 1 = 10.
+
+
+
+Extensions -
+
+- double or triple a letter
+- double or triple a word
+
+
---
+++ src/main/java/com/amaap/scrabblegame/Scrabble.java
@@ -0,0 +1,54 @@
+package com.amaap.scrabblegame;
+
+
+import com.amaap.scrabblegame.exceptions.InvalidStringException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class Scrabble {
+    private String word;
+    private Map<Integer, String> scoreMapping;
+    public Scrabble(String inputString) throws InvalidStringException {
+        if (inputString.trim().isEmpty()) {
+            throw new InvalidStringException(inputString + "Empty string is not allowed");
+        } else if (validateWord(inputString)) {
+            throw new InvalidStringException(inputString + " is an invalid string");
+        }
+        this.word = inputString.trim().toUpperCase();
+    }
+    private void createScoreManager() {
+        scoreMapping = new HashMap<>();
+        scoreMapping.put(1, "EAIONRTLSU");
+        scoreMapping.put(2, "DG");
+        scoreMapping.put(3, "BCMP");
+        scoreMapping.put(4, "FHVWY");
+        scoreMapping.put(5, "K");
+        scoreMapping.put(8, "JX");
+        scoreMapping.put(10, "QZ");
+
+    }
+    private boolean validateWord(String inputString) {
+        return Pattern.matches(".*[\\d\\W].*", inputString);
+    }
+    public int calculateScore() {
+        int score = 0;
+        createScoreManager();
+        for (int i = 0; i < word.length(); i++) {
+            score += getScore(word.charAt(i));
+        }
+        return score;
+    }
+    private int getScore(char c) {
+
+        return scoreMapping.entrySet().stream()
+                .filter(entry -> entry.getValue().contains(String.valueOf(c)))
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(0);
+
+    }
+}
+
+
